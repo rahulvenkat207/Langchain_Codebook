d8c15516f5270e4d9362678008a47b231404c3d7
rahulvenkat207 <rahulvenkat207@gmail.com>
2025-03-15 10:22:38 +0530

    Complete

--- 3_chains/2_chains_under_hood.py
+++ 3_chains/2_chains_under_hood.py
@@ -22,7 +22,7 @@ format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
 invoke_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
 parse_output = RunnableLambda(lambda x: x.content)
 
-# Create the RunnableSequence (equivalent to the LCEL chain)
+# (equivalent to the LCEL chain)
 chain = RunnableSequence(first=format_prompt, middle=[invoke_model], last=parse_output)
 
 # Run the chain
@@ -30,3 +30,9 @@ response = chain.invoke({"topic": "lawyers", "joke_count": 3})
 
 # Output
 print(response)
+"""1.  Why don't sharks attack lawyers?  Professional courtesy.
+
+2.  What's the difference between a good lawyer and a great lawyer?  A good lawyer knows the law. A great lawyer knows the judge.
+
+3. A lawyer is standing at the pearly gates. St. Peter looks at him and says, "You've lived a long life, but I see no good deeds that would let you into Heaven." The lawyer replies, "Well, I did get that guy off on a double homicide charge." St. Peter checks his notes. "Ah yes, I see that. But that was overturned on appeal." The lawyer smiles, "Not yet."    
+"""
